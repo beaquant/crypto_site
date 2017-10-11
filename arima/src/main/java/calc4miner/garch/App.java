6f28beeced85420af7ae5fb97c2aeff9d393e0c1
Vadim <muller95@yandex.ru>
2017-10-11 23:54:06 +0300

    arima calibration

--- arima/src/main/java/calc4miner/garch/App.java
+++ arima/src/main/java/calc4miner/garch/App.java
@@ -22,14 +22,12 @@ import timeseries.models.arima.ArimaSimulation;
  */
 public class App 
 {
-    public static void main( String[] args )
+    public static void pricePredictions()
     {
-        System.out.println( "Hello World!" );
-
         try {
             ArrayList<Double> vals = new ArrayList<Double>();
             ArrayList<Double> timeVals = new ArrayList<Double>();
-            BufferedReader reader = new BufferedReader(new FileReader("ethereum_price.csv"));
+            BufferedReader reader = new BufferedReader(new FileReader("data/ethereum_price.csv"));
             int count = 0;
             for(String line; (line = reader.readLine()) != null; count++) {
                 timeVals.add((double)count);
@@ -55,12 +53,13 @@ public class App
                                                             .build();
             // Arima model = Arima.model(timeSeries, coefficients, fittingStrategy);
             Arima model = Arima.model(timeSeries, ArimaOrder.order(90, 2, 2), fittingStrategy);
+            // Arima model = Arima.model(timeSeries, ArimaOrder.order(15, 1, 1), fittingStrategy);
             Forecast forecast = ArimaForecast.forecast(model, 364);
             double[] forecast_arr = forecast.forecast().asArray();
 
             System.out.println(forecast_arr.length);
 
-            File file = new File("price_predictions.csv");
+            File file = new File("data/price_predictions.csv");
             file.createNewFile();
             BufferedWriter writer = new BufferedWriter(new FileWriter(file));
             writer.write(String.format("%.20f\n", series[series.length - 1]));
@@ -70,5 +69,61 @@ public class App
         } catch (Exception e) {
             e.printStackTrace();
         }
+    }
+
+    public static void statsPredictions()
+    {
+        try {
+            ArrayList<Double> vals = new ArrayList<Double>();
+            ArrayList<Double> timeVals = new ArrayList<Double>();
+            BufferedReader reader = new BufferedReader(new FileReader("data/ethereum_stats.csv"));
+            int count = 0;
+            for(String line; (line = reader.readLine()) != null; count++) {
+                timeVals.add((double)count);
+                double val = Double.parseDouble(line.substring(line.indexOf(",") + 1));
+                vals.add(val);
+            }
+            reader.close();
+            double[] series = new double[vals.size()];
+            double[] time = new double[vals.size()];
+            for (int i = 0; i < vals.size(); i++) {
+                series[i] = vals.get(i);
+                time[i] = timeVals.get(i);
+            }
+
+            TimeSeries timeSeries = new TimeSeries(series);
+            Arima.FittingStrategy fittingStrategy = Arima.FittingStrategy.CSSML;
+            ArimaCoefficients coefficients = ArimaCoefficients.newBuilder()
+                                                            .setMACoeffs(-0.6760904)
+                                                            .setSeasonalMACoeffs(-0.5718134)
+                                                            .setDifferences(1)
+                                                            .setSeasonalDifferences(1)
+                                                            .setSeasonalFrequency(12)
+                                                            .build();
+            // Arima model = Arima.model(timeSeries, coefficients, fittingStrategy);
+            // Arima model = Arima.model(timeSeries, ArimaOrder.order(90, 2, 2), fittingStrategy);
+            Arima model = Arima.model(timeSeries, ArimaOrder.order(15, 1, 1), fittingStrategy);
+            Forecast forecast = ArimaForecast.forecast(model, 364);
+            double[] forecast_arr = forecast.forecast().asArray();
+
+            System.out.println(forecast_arr.length);
+
+            File file = new File("data/stats_predictions.csv");
+            file.createNewFile();
+            BufferedWriter writer = new BufferedWriter(new FileWriter(file));
+            writer.write(String.format("%.20f\n", series[series.length - 1]));
+            for (int i = 0; i < forecast_arr.length; i++) 
+                writer.write(String.format("%.20f\n", forecast_arr[i]));
+            writer.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+
+        pricePredictions();
+        statsPredictions();
     }
 }
